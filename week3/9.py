c5ad05ff3e79161bf349e6a092024488a8aa0aee
sardarbekd <78411709+sardarbekd@users.noreply.github.com>
2021-02-27 23:13:34 +0600

    Add files via upload

---
+++ week3/9.py
@@ -0,0 +1,10 @@
+# N, M = map(int, input().split())
+# a = set(map(int, input().split()))
+# b = set(map(int, input().split()))
+n, m = int(input()), int(input())
+
+for i in range(n + m):
+    x = int(input())
+
+# for i in range(N):
+    
