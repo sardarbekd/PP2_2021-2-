ebbfaf2a05ae0d8d0b88c1c1a6c7c87fd2768445
sardarbekd <78411709+sardarbekd@users.noreply.github.com>
2021-04-04 22:02:17 +0600

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,6 +1,12 @@
-a = 15
-b = 5
-print(a + b)
-print(int(a / b))
-print(a - b)
-print(a * b)
+import pygame 
+pygame.init()
+screen = pygame.display.set_mode((400, 300)) # parameters of the screen (width, length)
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip() # показывает изменения на экране
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(30, 30, 60, 60))
+    #                        (RED, GREEN, BLUE) можно возпользоваться colorspire.com
+# pressed= pygame.key.get_pressed() - список нажатых кнопок
--- 2.py
+++ 2.py
@@ -1,3 +1,20 @@
-n = int(input())
-a = 2048
-print(int(a**(1/n)))
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+
+is_blue = True
+color = (0, 0, 255)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_blue = not is_blue
+
+    pygame.display.flip()
+
+    if not is_blue : 
+        color = 
